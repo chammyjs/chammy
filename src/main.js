ee45974d53ff2d42596c26871bda15d7bf5a370f
Kornelia Kobiela <korneliak.95@gmail.com>
2017-11-21 16:07:16 +0100

    Clean dependencies and add CLI features

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 require( 'babel-register' );
-const term = require( 'terminal-kit' ).terminal ;
-
-term.grabInput( { mouse: 'button' } ) ;
-
-term.on( 'mouse', ( name, data ) => {
-	    console.log( '\'mouse\' event:', name, data ) ;
-} ) ;
+module.exports = function(  ) {
+// TODO:options handler
+};
